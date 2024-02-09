da9e5173963220b5a2a526d0f1776733416b731c
DreJones06 <157946805+DreJones06@users.noreply.github.com>
2024-02-09 23:25:57 +0530

    Update array_script.sh

--- array_script.sh
+++ array_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Define an array
 fruits=("Apple" "Banana" "Orange")
-# Access elements of the array
+# Access elements of the array hello
 echo "First fruit: ${fruits[0]}"
 echo "All fruits: ${fruits[@]}"
